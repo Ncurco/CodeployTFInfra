34b58d9fe7e9a6087561a7bee94bae8b359451ae
Ncurco <32915881+Ncurco@users.noreply.github.com>
2020-12-01 19:58:28 -0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-VIRTUALENV=/home/ubuntu/venv
-
-pushd $(dirname "$0")
-source $VIRTUALENV/bin/activate
+cd /home/ubuntu
 export FLASK_APP=app.py
-flask run --host=0.0.0.0 --port 8080
-deactivate
-popd
+flask run --host=0.0.0.0 --port 8080 >/dev/null 2>&1 &
 
