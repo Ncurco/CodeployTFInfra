054ee48c524c77142025968f6a63fdddd45003b2
Ncurco <32915881+Ncurco@users.noreply.github.com>
2020-12-01 18:00:36 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,9 @@
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip
+mkdir venv
+virtualenv venv
+source venv/bin/activat
 pip install Flask
 pip install flask-wtf
 pip install boto3
