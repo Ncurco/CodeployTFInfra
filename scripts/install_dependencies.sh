897540e6fee72d96e8ffb76b041b0b81cd257d44
Ncurco <32915881+Ncurco@users.noreply.github.com>
2020-12-01 17:44:22 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,8 +4,5 @@ cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip
 pip install Flask
-ls
-cd deployment
-ls
-pip install -r requirements.txt
-sudo cp etc/rc.local /etc/
+pip install flask-wtf
+pip install boto3
