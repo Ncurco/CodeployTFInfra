99467fdac4f8365d0f925d65a65991c6a219f469
Ncurco <32915881+Ncurco@users.noreply.github.com>
2020-12-01 17:14:29 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,8 +3,5 @@
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip
-mkdir venv
-virtualenv venv
-source venv/bin/activate
 pip install Flask
 pip install -r requirements.txt
